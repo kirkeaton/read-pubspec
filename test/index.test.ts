0872060d156449586529ae50b1621f9fb5d492ea
Kirk Eaton <contact@kirkeaton.ca>
2025-08-17 09:26:44 -0400

    chore: Add more fields to test files

--- test/index.test.ts
+++ test/index.test.ts
@@ -13,10 +13,26 @@ test('should read pubspec.yaml from current working directory', async (t) => {
   const pubspec = await readPubspec();
   assert.equal(pubspec.name, 'read_pubspec');
   assert.equal(pubspec.version, '1.0.0');
+  assert.equal(
+    pubspec.description,
+    'A sample project for testing pubspec.yaml parsing.'
+  );
+  assert.equal(pubspec.environment.sdk, '>=2.12.0 <3.0.0');
+  assert.equal(pubspec.dependencies?.http, '^0.13.0');
+  assert.equal(pubspec.dependencies?.path, '^1.8.0');
+  assert.equal(pubspec.dev_dependencies?.test, '^1.16.0');
 });
 
 test('should read pubspec.yaml from directory provided by cwd option', async (t) => {
   const pubspec = await readPubspec({ cwd: rootCwd });
   assert.equal(pubspec.name, 'read_pubspec');
   assert.equal(pubspec.version, '1.0.0');
+  assert.equal(
+    pubspec.description,
+    'A sample project for testing pubspec.yaml parsing.'
+  );
+  assert.equal(pubspec.environment.sdk, '>=2.12.0 <3.0.0');
+  assert.equal(pubspec.dependencies?.http, '^0.13.0');
+  assert.equal(pubspec.dependencies?.path, '^1.8.0');
+  assert.equal(pubspec.dev_dependencies?.test, '^1.16.0');
 });
